fcff71c88587546810b7d8928979322c171b23b0
Abhishek Singh <abhishek.nitdgp.98@gmail.com>
2021-03-04 16:34:51 +0530

    added img display snippet

--- run.py
+++ run.py
@@ -5,6 +5,13 @@ from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 from PIL import Image
 
+import io
+import base64
+
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+
 
 UPLOAD_FOLDER = 'uploads/'
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -22,7 +29,7 @@ app.config['UPLOAD_EXTENSIONS'] = ['.wav']
 def home():
 	return render_template('home.html')
 
-@app.route('/upload', methods = ['GET', 'POST'])
+@app.route('/upload', methods = ['GET'])
 def upload_file():
 	if request.method == 'POST':
 		uploaded_file = request.files['file']
@@ -35,6 +42,27 @@ def upload_file():
 		
 		return redirect(url_for('home'))
 	return render_template('upload.html')
+
+@app.route('/upload', methods = ['POST'])
+def display_spectrogram():
+	# Generate plot
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    axis.set_title("title")
+    axis.set_xlabel("x-axis")
+    axis.set_ylabel("y-axis")
+    axis.grid()
+    axis.plot(range(5), range(5), "ro-")
+
+    # Convert plot to PNG image
+    pngImage = io.BytesIO()
+    FigureCanvas(fig).print_png(pngImage)
+
+    # Encode PNG image to base64 string
+    pngImageB64String = "data:image/png;base64,"
+    pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
+
+    return render_template("home.html", image=pngImageB64String)
 
 @app.route('/about')
 def about():
